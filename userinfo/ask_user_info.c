1c8682f52045ab3b83ec27cdeb3e39214a6346af
Videl Smith <thbtsmith@gmail.com>
2013-05-12 08:20:24 +0200

    Ajout du bandeau GNU99-compatible :)

--- userinfo/ask_user_info.c
+++ userinfo/ask_user_info.c
@@ -1,3 +1,7 @@
+/**
+ * Code compatible avec *GNU99*
+ **/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "network_generation.h"
--- userinfo/network_generation.c
+++ userinfo/network_generation.c
@@ -1,6 +1,7 @@
 /**
  * Code compatible avec *GNU99*
  **/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
