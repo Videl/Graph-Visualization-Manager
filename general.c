12b2e8ccdd164673542e38b185410de74c83c3dd
Videl Smith <thbtsmith@gmail.com>
2013-05-15 22:13:44 +0200

    Fix: IHM

--- general.c
+++ general.c
@@ -8,8 +8,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char input[MAX_NAME];
 
 int main(void)
 { 
@@ -42,12 +42,44 @@ int main(void)
         default:
         break;
     }
-    printf("Indiquez le nom du fichier texte contenant les informations : ");
+
+    char link[300] = "./dot_maker/text_to_dot ";
+    char link2[300] = "dot -T png -o ";
+    char input[50];
+    char input2[50];
+    char input3[50];
+
+    printf("Indiquez le nom du fichier texte contenant les informations :\n");
+    printf("(Le lien complet à partir de la racine du projet) : ");
+
     scanf("%s", input);
     getchar();
+
+    strcat(link, input);
+
+    printf("Indiquer le nom du fichier .dot en sortie : ");
+    scanf("%s", input2);
+    getchar();
+
+    strcat(link, " ");
+    strcat(link, input2);
+
+    printf("%s... ", link);
+    system(link);
+    printf("exécuté.\n");
+
+
+    printf("Indiquer le nom du fichier image (.png) en sortie : ");
+    scanf("%s", input3);
+    getchar();
+
+    strcat(link2, input3);
+    strcat(link2, " ");
+    strcat(link2, input2);
+
+    printf("%s...", link2);
+    system(link2);
     
-    //system(./dot_maker/text_to_dot reseau/input.txt output.dot);
-    //system(dot -t png -o output.png output.dot);
-    
+    printf("exécuté.\n");
     
 }
