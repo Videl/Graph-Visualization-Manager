3b69c8dec26baa5a74a1fabdcf1a4836d0bff5ce
Videl Smith <thbtsmith@gmail.com>
2013-05-15 15:38:32 +0200

    Makefile récursif

--- Makefile
+++ Makefile
@@ -1,6 +1,24 @@
-MODULES = learning Reseau userinfo
+CC=gcc
+CFLAGS=-W -Wall -std=gnu99 $(DEBUG)
+LFLAGS=
+EXEC=general
+SUBDIRS=dot_maker reseau userinfo
 
-all:
-    for dir in $(MODULES); do
-        (cd $$dir; ${MAKE} all);
-        done
+
+all: dot_maker reseau userinfo $(EXEC).o
+	$(CC) -o $(EXEC) $(EXEC).o $(LFLAGS)
+
+$(EXEC).o: 
+	$(CC) -c $(EXEC).c $(CFLAGS)
+
+$(SUBDIRS):
+	$(MAKE) -C $@
+
+.PHONY: clean mrproper dot_maker reseau userinfo
+
+clean:
+	@rm -rf *.o
+	@rm -rf *.core
+
+mrproper: clean
+	@rm -rf $(EXEC)
--- general.c
+++ general.c
@@ -7,38 +7,38 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int main (int argc, char **argv)
-{
-    
+int main(void)
+{ 
     int choix;
     
-    printf(" ====================\n");
-    printf(" Projet C : \n");
-    printf(" Visualisation d'un réseau de transport \n");
-    printf(" ====================\n");
+    printf("###############################################\n");
+    printf("#                   Projet C                  #\n");
+    printf("#    Visualisation d'un réseau de transport   #\n");
+    printf("###############################################\n");
     
-   printf("Comment voulez-vous utiliser ce programme? \n");
+   printf("Comment voulez-vous utiliser ce programme ?\n");
     
     do {
-        printf("1: Utiliser un réseau deja existant \n");
-        printf("2: Creer son propre reseau \n");
-        scanf("%i", &choix);
+        printf("1. Utiliser un réseau deja existant\n");
+        printf("2. Créer son propre reseau\n");
+        printf("%% ");
+        scanf("%d", &choix);
+        getchar();
     } while (choix<=0 || choix>2);
     
     switch (choix) {
         case 1:
-            system("./Reseau/main");
+            system("cd reseau && ./main");
             break;
             
         case 2:
-            system("./userinfo/ask_user_info");
+            system("cd userinfo/ && ./ask_user_info");
             break;
             
         default:
-            system("./Reseau/main");
-            break;
+        break;
     }
     
     
